0127316fdd9221b29d8fa696bde4ab7ed46edc6c
karin1217 <1946294910@qq.com>
2018-04-08 14:46:17 +0000

    Updated postgresql configuration

--- config/database.php
+++ config/database.php
@@ -67,19 +67,19 @@ return [
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
-            'sslmode' => 'prefer',
+            'sslmode' => 'require',
         ],
 
-        'sqlsrv' => [
-            'driver' => 'sqlsrv',
-            'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', '1433'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => 'utf8',
-            'prefix' => '',
-        ],
+//        'sqlsrv' => [
+//            'driver' => 'sqlsrv',
+//            'host' => env('DB_HOST', 'localhost'),
+//            'port' => env('DB_PORT', '1433'),
+//            'database' => env('DB_DATABASE', 'forge'),
+//            'username' => env('DB_USERNAME', 'forge'),
+//            'password' => env('DB_PASSWORD', ''),
+//            'charset' => 'utf8',
+//            'prefix' => '',
+//        ],
 
     ],
 
