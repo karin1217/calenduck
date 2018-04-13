3be514bfaaa1b6731f2a72f73199cc326744a189
karin1217 <1946294910@qq.com>
2018-04-13 15:21:38 +0000

    Check the errors on Heroku

--- app/Http/Controllers/StaticPagesController.php
+++ app/Http/Controllers/StaticPagesController.php
@@ -9,6 +9,7 @@ class StaticPagesController extends Controller
 {
     public function home()
     {
+        var_dump('Hello');exit;
 
         //$users = User::all();
         //var_dump($users);exit;
